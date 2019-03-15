608eca0a6bb7d58297838f256a2d8e9ab2e376e9
nathanestill <nathan.estill@students.olin.edu>
2019-03-15 17:45:49 -0400

    adding waypoint rough draft of overview

---
+++ scripts/waypoint.py
@@ -0,0 +1,22 @@
+def go(waypoints):
+	# do while loop
+	# while?(is it there)
+	# 	increment waypoint
+	# passonTwist
+	# while there are still waypoints left
+	# done?
+	# returns something idk
+def isitThere(currentOdom, nextWaypoint):
+	# calcVecOdom
+	# is the vec less than a specified threshold?
+	# returns boolean
+def passOnTwist(currentOdom, nextWaypoint):
+	# something like:
+	# calculate the vector from odom to the waypoint
+	# do trig to calc the steering angle
+	# maybe need to specify speed, but how?
+	# publish the twist? or ackermann to the MainState
+	# returns nothing
+
+def calculateVectorFromOdomToWaypoint(currentOdom,nextWaypoint):
+	# vector math

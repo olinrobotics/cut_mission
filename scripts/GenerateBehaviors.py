2565bd5cd40662813e66e7ff93002e2dd3f92a25
Connor <connorrnovak@gmail.com>
2019-04-03 16:15:23 -0400

    Change behavior file generation - Layout p2pbehavior node

--- scripts/GenerateBehaviors.py
+++ scripts/GenerateBehaviors.py
@@ -5,8 +5,9 @@ import os
 
 mission_file = '../config/p2p_test.yaml'
 behavior_file = '../config/behaviors.yaml'
-behaviors = []
+behaviors = {'safety':'0', 'teleop':'1'}
 
+# Load mission behaviors
 with open(mission_file, 'r') as f:
     doc = yaml.load(f)
     print("Loading mission behaviors: " + doc['title'])
@@ -14,31 +15,37 @@ with open(mission_file, 'r') as f:
         behavior = doc['waypoints'][i]['behavior']
         if not behavior in behaviors:
             print("New behavior: " + behavior)
-            behaviors.append(behavior)
+            behaviors[behavior] = '2'
 
 overwrite = False
+confirm_ans = ['y', 'Y', 'yes', 'Yes']
+deny_ans = ['n', 'N', 'no,' 'No']
 
+# Get user input on overwriting files
 if os.path.isfile(behavior_file):
     while(True):
         input = raw_input(behavior_file + ' already exists - would you like to overwrite it?\n Input: ')
-        if input == 'y' or input == 'Y' or input == 'Yes' or input == 'yes':
+        if input in confirm_ans:
             print("Overwriting " + behavior_file)
             overwrite = True
             break
-        elif input == 'n' or input == 'N' or input == 'No' or input == 'no':
+        elif input in deny_ans:
             print("Canceling operation")
             break
         else:
             print("Erroneous response: please use y or n")
 
+# Regenerate new file to replace overwritten file
 if overwrite:
     os.remove(behavior_file)
+
+    # Generate yaml formatted behaviors
     data = dict()
-    index = 0
     for behavior in behaviors:
         param = '/behaviors/' + behavior
-        data[param] = index
-        index += 1
+        data[param] = behaviors[behavior]
+
+    # Write header comments, dump data into file
     with open(behavior_file, "w") as f:
         f.write('# Autogen behavior file from GenerateBehaviors.py\n')
         f.write('# behaviors/[name_of_behavior]: \'[priority]\'\n')
